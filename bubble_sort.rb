9c370c9826f52a50d30a56e5ba1393b160a99857
Bello S Babakolo <fleetbeekay@gmail.com>
2020-09-03 14:10:53 +0100

    Optimize bubble sort using flag.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,29 @@
 def bubble_sort(arr)
   arr.each_index do |_|
+    swapped = false
     arr.each_index do |i|
-      arr[i], arr[i + 1] = arr[i + 1], arr[i] if i < arr.length - 1 && arr[i] > arr[i + 1]
+      if (i < arr.length - 1) && (arr[i] > arr[i + 1])
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
     end
+    if !swapped then break end;
   end
   arr
 end
 
 def bubble_sort_by(arr)
   arr.each_index do |_|
+    swapped = false
     arr.each_index do |i|
       if i < arr.length - 1
-        arr[i], arr[i + 1] = arr[i + 1], arr[i] if yield(arr[i], arr[i + 1]).positive?
+        if yield(arr[i], arr[i + 1]).positive?
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+          swapped = true
+        end
       end
     end
+    if !swapped then break end;
   end
   arr
 end
